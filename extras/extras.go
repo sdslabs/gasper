3bb2201495ea18d964e278c9299c09546ea4dcce
Vaibhav <vrongmeal@gmail.com>
2018-09-28 16:38:54 +0530

    chore(extras): add package for extra definitions and functions
    include `Error` type for response errors

---
+++ extras/extras.go
@@ -0,0 +1,12 @@
+package extras
+
+// Error is a type for declaring a response error from server
+type Error struct {
+	Code int // status code of the error
+	Err  error
+}
+
+// Reason is a method for type `Error` which returns the reson for the non 200 status code
+func (e *Error) Reason() string {
+	return e.Err.Error()
+}
