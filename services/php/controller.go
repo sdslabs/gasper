7057a2e9aec89a46cc8534937e86584a0298c8ce
Supratik Das <rick.das08@gmail.com>
2018-12-08 01:50:09 +0530

    feat(services/php): addition of php microsevice to handle php apps

--- services/php/controller.go
+++ services/php/controller.go
@@ -1,1 +1,58 @@
 package php
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sdslabs/SWS/lib/utils"
+)
+
+// phpAppConfig is json binding config for creating new php page
+type phpAppConfig struct {
+	Name      string `json:"name" form:"name" binding:"required"`
+	UserID    int    `json:"user_id" form:"user_id" binding:"required"`
+	GithubURL string `json:"github_url" form:"github_url" binding:"required"`
+}
+
+// readAndWriteConfig creates new config file for the given app
+func (json phpAppConfig) ReadAndWriteConfig() utils.Error {
+	// containerID, ok := os.LookupEnv("PHP_CONTAINER_ID")
+	// if !ok {
+	// 	return utils.Error{
+	// 		Code: 500,
+	// 		Err:  errors.New("PHP_CONTAINER_ID not found in the environment"),
+	// 	}
+	// }
+
+	err := utils.ReadAndWriteConfig(json.Name, "php", "3b99fa7534c3")
+	if err != nil {
+		return utils.Error{
+			Code: 500,
+			Err:  err,
+		}
+	}
+
+	return utils.Error{
+		Code: 200,
+		Err:  nil,
+	}
+}
+
+// createApp function handles requests for making making new php app
+func createApp(c *gin.Context) {
+	var (
+		json phpAppConfig
+		err  utils.Error
+	)
+	c.BindJSON(&json)
+
+	err = json.ReadAndWriteConfig()
+	if err.Code != 200 {
+		c.JSON(err.Code, gin.H{
+			"message": err.Reason(),
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"success": true,
+	})
+}
--- services/php/routes.go
+++ services/php/routes.go
@@ -1,1 +1,15 @@
 package php
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Router is the main routes handler for the current microservice package
+var Router = gin.Default()
+
+func init() {
+	appGroup := Router.Group("/php")
+	{
+		appGroup.POST("/create", createApp)
+	}
+}
