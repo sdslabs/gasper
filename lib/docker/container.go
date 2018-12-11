8495f7d9e76ee146e13293c1c6d29c517ba7a86e
Alphadose <anish.mukherjee1996@gmail.com>
2018-12-12 01:06:24 +0530

    refactor(lib): made port names meaningful

--- lib/docker/container.go
+++ lib/docker/container.go
@@ -9,7 +9,7 @@ import (
 )
 
 // CreateContainer creates a new container of the given container options, returns id of the container created
-func CreateContainer(ctx context.Context, cli *client.Client, image, port80, port22, workDir, name string) (string, error) {
+func CreateContainer(ctx context.Context, cli *client.Client, image, httpPort, sshPort, workDir, name string) (string, error) {
 	containerConfig := &container.Config{
 		WorkingDir: workDir,
 		Image:      image,
@@ -23,8 +23,8 @@ func CreateContainer(ctx context.Context, cli *client.Client, image, port80, por
 			"/var/run/docker.sock:/var/run/docker.sock",
 		},
 		PortBindings: nat.PortMap{
-			nat.Port("80/tcp"): []nat.PortBinding{{HostIP: "0.0.0.0", HostPort: port80}},
-			nat.Port("22/tcp"): []nat.PortBinding{{HostIP: "0.0.0.0", HostPort: port22}},
+			nat.Port("80/tcp"): []nat.PortBinding{{HostIP: "0.0.0.0", HostPort: httpPort}},
+			nat.Port("22/tcp"): []nat.PortBinding{{HostIP: "0.0.0.0", HostPort: sshPort}},
 		},
 	}
 	createdConf, err := cli.ContainerCreate(ctx, containerConfig, hostConfig, nil, name)
