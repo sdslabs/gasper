8800579554b9b8ed5d35031b8411a8c71dd2ccf0
Supratik Das <rick.das08@gmail.com>
2018-12-22 12:08:22 +0530

    feat(services/php/controller.go): add basic functions to deploy php app

--- services/php/controller.go
+++ services/php/controller.go
@@ -2,57 +2,67 @@ package php
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/sdslabs/SWS/lib/utils"
+	"github.com/sdslabs/SWS/lib/mongo"
 )
-
-// phpAppConfig is json binding config for creating new php page
-type phpAppConfig struct {
-	Name      string `json:"name" form:"name" binding:"required"`
-	UserID    int    `json:"user_id" form:"user_id" binding:"required"`
-	GithubURL string `json:"github_url" form:"github_url" binding:"required"`
-}
-
-// readAndWriteConfig creates new config file for the given app
-func (json phpAppConfig) ReadAndWriteConfig() utils.Error {
-	// containerID, ok := os.LookupEnv("PHP_CONTAINER_ID")
-	// if !ok {
-	// 	return utils.Error{
-	// 		Code: 500,
-	// 		Err:  errors.New("PHP_CONTAINER_ID not found in the environment"),
-	// 	}
-	// }
-
-	err := utils.ReadAndWriteConfig(json.Name, "php", "3b99fa7534c3")
-	if err != nil {
-		return utils.Error{
-			Code: 500,
-			Err:  err,
-		}
-	}
-
-	return utils.Error{
-		Code: 200,
-		Err:  nil,
-	}
-}
 
 // createApp function handles requests for making making new php app
 func createApp(c *gin.Context) {
 	var (
-		json phpAppConfig
-		err  utils.Error
+		data map[string]interface{}
 	)
-	c.BindJSON(&json)
-
-	err = json.ReadAndWriteConfig()
-	if err.Code != 200 {
-		c.JSON(err.Code, gin.H{
-			"message": err.Reason(),
-		})
-		return
-	}
+	c.BindJSON(&data)
+	data["language"] = "php"
 
 	c.JSON(200, gin.H{
 		"success": true,
+		"id":      mongo.RegisterApp(data),
 	})
+}
+
+func fetchDocs(c *gin.Context) {
+	queries := c.Request.URL.Query()
+	filter := queryToFilter(queries)
+
+	filter["language"] = "php"
+
+	c.JSON(200, gin.H{
+		"data": mongo.FetchAppInfo(filter),
+	})
+}
+
+func deleteApp(c *gin.Context) {
+	queries := c.Request.URL.Query()
+	filter := queryToFilter(queries)
+
+	filter["language"] = "php"
+
+	c.JSON(200, gin.H{
+		"message": mongo.DeleteApp(filter),
+	})
+}
+
+func updateApp(c *gin.Context) {
+	queries := c.Request.URL.Query()
+	filter := queryToFilter(queries)
+
+	filter["language"] = "php"
+
+	var (
+		data map[string]interface{}
+	)
+	c.BindJSON(&data)
+
+	c.JSON(200, gin.H{
+		"message": mongo.UpdateApp(filter, data),
+	})
+}
+
+func queryToFilter(queries map[string][]string) map[string]interface{} {
+	filter := make(map[string]interface{})
+
+	for key, value := range queries {
+		filter[key] = value[0]
+	}
+
+	return filter
 }
