13688ee0b29af4c493198161d34879852b32598f
Alphadose <anish.mukherjee1996@gmail.com>
2018-12-26 20:11:33 +0530

    feat(mongo): return error if any on document insertion

--- lib/mongo/create.go
+++ lib/mongo/create.go
@@ -8,18 +8,18 @@ import (
 )
 
 // InsertOne inserts a document into a mongoDB collection
-func InsertOne(collectionName string, data bson.M) interface{} {
+func InsertOne(collectionName string, data bson.M) (interface{}, error) {
 	collection := link.Collection(collectionName)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	res, err := collection.InsertOne(ctx, data)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return res.InsertedID
+	return res.InsertedID, nil
 }
 
 // RegisterApp is an abstraction over InsertOne which inserts application info into mongoDB
-func RegisterApp(data bson.M) interface{} {
+func RegisterApp(data bson.M) (interface{}, error) {
 	return InsertOne("apps", data)
 }
