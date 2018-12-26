a134b2487694529b183816ba6b69440600caa077
Alphadose <anish.mukherjee1996@gmail.com>
2018-12-26 20:12:53 +0530

    feat(static): add clone and container creation functionalities on POST endpoint

--- services/static/controller.go
+++ services/static/controller.go
@@ -1,8 +1,13 @@
 package static
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
+	"github.com/sdslabs/SWS/lib/api"
+	"github.com/sdslabs/SWS/lib/configs"
 	"github.com/sdslabs/SWS/lib/mongo"
+	"github.com/sdslabs/SWS/lib/types"
 	"github.com/sdslabs/SWS/lib/utils"
 )
 
@@ -12,11 +17,60 @@ func createApp(c *gin.Context) {
 		data map[string]interface{}
 	)
 	c.BindJSON(&data)
+
+	ports, err := utils.GetFreePorts(2)
+
+	if err != nil {
+		c.JSON(200, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	if len(ports) < 2 {
+		c.JSON(200, gin.H{
+			"error": "Not Enough Ports",
+		})
+		return
+	}
+
+	sshPort := ports[0]
+	httpPort := ports[1]
+
+	containerID, err := api.CreateBasicApplication(
+		data["name"].(string),
+		data["url"].(string),
+		strconv.Itoa(httpPort),
+		strconv.Itoa(sshPort),
+		&types.ApplicationConfig{
+			DockerImage:  "nginx",
+			ConfFunction: configs.CreateStaticContainerConfig,
+		})
+
+	if err != nil {
+		c.JSON(200, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	data["sshPort"] = sshPort
+	data["httpPort"] = httpPort
+	data["containerID"] = containerID
 	data["language"] = "static"
+
+	documentID, err := mongo.RegisterApp(data)
+
+	if err != nil {
+		c.JSON(200, gin.H{
+			"error": err,
+		})
+		return
+	}
 
 	c.JSON(200, gin.H{
 		"success": true,
-		"id":      mongo.RegisterApp(data),
+		"id":      documentID,
 	})
 }
 
