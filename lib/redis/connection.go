6d85616239fffd44ebe0b71549535955344d050f
Alphadose <anish.mukherjee1996@gmail.com>
2018-12-28 16:07:38 +0530

    fix(redis/connection): explicit type conversion of DB parameter into int

--- lib/redis/connection.go
+++ lib/redis/connection.go
@@ -10,7 +10,7 @@ import (
 var client = redis.NewClient(&redis.Options{
 	Addr:     utils.RedisConfig["host"].(string) + utils.RedisConfig["port"].(string),
 	Password: utils.RedisConfig["password"].(string),
-	DB:       utils.RedisConfig["DB"].(int),
+	DB:       int(utils.RedisConfig["DB"].(float64)),
 })
 
 func init() {
