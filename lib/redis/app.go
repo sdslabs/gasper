24be78d598e7922e3539fe26e2ac3185e9f3c415
Alphadose <anish.mukherjee1996@gmail.com>
2018-12-29 12:06:26 +0530

    feat(lib/redis): add functionality to register apps in bulk

--- lib/redis/app.go
+++ lib/redis/app.go
@@ -6,6 +6,15 @@ func RegisterApp(appName, url string) error {
 	return err
 }
 
+// BulkRegisterApps registers multiple apps at once
+func BulkRegisterApps(data map[string]interface{}) error {
+	if len(data) == 0 {
+		return nil
+	}
+	_, err := client.HMSet("apps", data).Result()
+	return err
+}
+
 // FetchAppURL returns the URL of the machine where the app in query is deployed
 func FetchAppURL(appName string) (string, error) {
 	result, err := client.HGet("apps", appName).Result()
