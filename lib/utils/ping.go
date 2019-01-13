ab712f609c4b14b87c15418dddc0029c488a4915
Alphadose <anish.mukherjee1996@gmail.com>
2019-01-13 19:26:14 +0530

    feat(utils/ping): update ping function from GET requests to net.Dial

--- lib/utils/ping.go
+++ lib/utils/ping.go
@@ -2,15 +2,18 @@ package utils
 
 import (
 	"fmt"
-	"net/http"
+	"net"
+	"time"
 )
 
 // NotAlive checks if a given instance is alive or not
 func NotAlive(url string) bool {
-	_, err := http.Get(url)
-	fmt.Println(err)
+	d := net.Dialer{Timeout: 5 * time.Second}
+	conn, err := d.Dial("tcp", url)
 	if err != nil {
+		fmt.Println(err)
 		return true
 	}
+	conn.Close()
 	return false
 }
