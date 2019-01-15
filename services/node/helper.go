307dad327f88fba498f47b4f2c472e78ece6998e
Alphadose <anish.mukherjee1996@gmail.com>
2019-01-15 06:30:59 +0530

    feat(node): direct stdout and stderr to logs

--- services/node/helper.go
+++ services/node/helper.go
@@ -7,7 +7,7 @@ import (
 
 // installPackages function installs the dependancies for the app
 func installPackages(appEnv *types.ApplicationEnv) (string, types.ResponseError) {
-	cmd := []string{"bash", "-c", `export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && npm install`}
+	cmd := []string{"bash", "-c", `export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; npm install &> /proc/1/fd/1`}
 	execID, err := docker.ExecDetachedProcess(appEnv.Context, appEnv.Client, appEnv.ContainerID, cmd)
 	if err != nil {
 		return "", types.NewResErr(500, "Failed to perform npm install in the container", err)
@@ -17,7 +17,7 @@ func installPackages(appEnv *types.ApplicationEnv) (string, types.ResponseError)
 
 // startApp function starts the app using pm2
 func startApp(index string, appEnv *types.ApplicationEnv) (string, types.ResponseError) {
-	cmd := []string{"bash", "-c", `export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && pm2 start ` + index}
+	cmd := []string{"bash", "-c", `export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; pm2 start ` + index + ` &> /proc/1/fd/1`}
 	execID, err := docker.ExecDetachedProcess(appEnv.Context, appEnv.Client, appEnv.ContainerID, cmd)
 	if err != nil {
 		return "", types.NewResErr(500, "Failed to perform start app in the container", err)
