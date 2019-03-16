46a2b95fbd64d58118f207be2310d56ec1057f09
Vaibhav <vrongmeal@gmail.com>
2019-03-16 23:18:46 +0530

    feat(ssh): add BuildSSHServer method
    this method creates an ssh server for the config given;
    also add sample config

--- services/ssh/controller.go
+++ services/ssh/controller.go
@@ -2,6 +2,7 @@ package ssh
 
 import (
 	"github.com/gliderlabs/ssh"
+	"github.com/sdslabs/SWS/lib/utils"
 )
 
 // Handler handles the ssh session.
@@ -15,4 +16,22 @@ func PublicKeyHandler(ctx ssh.Context, key ssh.PublicKey) bool {
 // PasswordHandler handles the password authentication
 func PasswordHandler(ctx ssh.Context, password string) bool {
 	return true
+}
+
+// BuildSSHServer creates a server for the given parameters
+func BuildSSHServer() (*ssh.Server, error) {
+	sshConfig := utils.ServiceConfig["ssh"].(map[string]interface{})
+	filepaths := utils.ToStringSlice(sshConfig["host_signers"])
+	hostSigners, err := getHostSigners(filepaths)
+	if err != nil {
+		return nil, err
+	}
+	return &ssh.Server{
+		Addr:        sshConfig["port"].(string),
+		HostSigners: hostSigners,
+
+		Handler:          Handler,
+		PasswordHandler:  PasswordHandler,
+		PublicKeyHandler: PublicKeyHandler,
+	}, nil
 }
