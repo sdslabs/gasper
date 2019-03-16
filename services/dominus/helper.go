e406cab70e5f7c38f3a0c51d7d483e3d4bf2baca
Alphadose <anish.mukherjee1996@gmail.com>
2019-03-17 04:36:38 +0530

    feat(dominus): implement load balancing

---
+++ services/dominus/helper.go
@@ -0,0 +1,23 @@
+package dominus
+
+import (
+	"net/http"
+	"net/http/httputil"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sdslabs/SWS/lib/utils"
+)
+
+func reverseProxy(c *gin.Context, target string) {
+	director := func(req *http.Request) {
+		req.URL.Scheme = "http"
+		req.URL.Host = target
+		req.Host = target
+		req.Header["dominus-secret"] = []string{utils.SWSConfig["secret"].(string)}
+		if req.Method == "POST" {
+			req.URL.Path = ""
+		}
+	}
+	proxy := &httputil.ReverseProxy{Director: director}
+	proxy.ServeHTTP(c.Writer, c.Request)
+}
