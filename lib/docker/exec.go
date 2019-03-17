fa16c3b6334c6043a7f7b190be4ecc749b3de6c2
Vaibhav <vrongmeal@gmail.com>
2019-03-17 16:48:21 +0530

    fix(lib/docker/exec.go): check if exec ID is empty before returning success

--- lib/docker/exec.go
+++ lib/docker/exec.go
@@ -1,6 +1,8 @@
 package docker
 
 import (
+	"errors"
+
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
@@ -9,6 +11,7 @@ import (
 // ExecDetachedProcess executes a command in detached form, returns the id of the process
 // Command of the exec format: mkdir folder => ["mkdir", "folder"]
 func ExecDetachedProcess(ctx context.Context, cli *client.Client, containerID string, command []string) (string, error) {
+	// TODO: check if container is up and running first
 	config := types.ExecConfig{
 		Detach: true,
 		Cmd:    command,
@@ -18,6 +21,9 @@ func ExecDetachedProcess(ctx context.Context, cli *client.Client, containerID st
 		return "", err
 	}
 	execID := execProcess.ID
+	if execID == "" {
+		return "", errors.New("empty exec ID")
+	}
 	err = cli.ContainerExecStart(ctx, execID, types.ExecStartCheck{Detach: true})
 	if err != nil {
 		return "", err
