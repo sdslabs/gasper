6cbe8d1b5cd12e693ee6e073d1802bbd3da797fe
Vaibhav <vrongmeal@gmail.com>
2019-03-17 16:38:20 +0530

    feat(ssh): add setWinsize helper to resize PTY device

--- services/ssh/helper.go
+++ services/ssh/helper.go
@@ -2,6 +2,9 @@ package ssh
 
 import (
 	"io/ioutil"
+	"os"
+	"syscall"
+	"unsafe"
 
 	"github.com/gliderlabs/ssh"
 	gossh "golang.org/x/crypto/ssh"
@@ -51,4 +54,11 @@ func getPublicKey(filepath string) (ssh.PublicKey, error) {
 	}
 
 	return publicKey.(ssh.PublicKey), nil
+}
+
+// setWinsize uses low-level system call to resize the PTY device "which is just a FD in unix systems".
+// See -- https://github.com/gliderlabs/ssh/blob/master/_examples/ssh-pty/pty.go
+func setWinsize(f *os.File, w, h int) {
+	syscall.Syscall(syscall.SYS_IOCTL, f.Fd(), uintptr(syscall.TIOCSWINSZ),
+		uintptr(unsafe.Pointer(&struct{ h, w, x, y uint16 }{uint16(h), uint16(w), 0, 0})))
 }
