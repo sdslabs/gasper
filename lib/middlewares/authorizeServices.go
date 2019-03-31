ffa3c9ad9172878d7a755ce9d9a479e791f4a645
Alphadose <anish.mukherjee1996@gmail.com>
2019-03-31 10:56:06 +0530

    feat(gin/middlewares): add a request body validation middleware

--- lib/middlewares/authorizeServices.go
+++ lib/middlewares/authorizeServices.go
@@ -1,6 +1,12 @@
 package middlewares
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/sdslabs/SWS/lib/utils"
 )
@@ -23,5 +29,36 @@ func AuthorizeService() gin.HandlerFunc {
 			return
 		}
 		c.Next()
+	}
+}
+
+// ValidateParams checks whether the required parameters are present in the Request Body
+func ValidateParams(paramList []string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var bodyBytes []byte
+		if c.Request.Body != nil {
+			bodyBytes, _ = ioutil.ReadAll(c.Request.Body)
+		}
+		// Restore the io.ReadCloser to its original state
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+		var req map[string]interface{}
+		json.Unmarshal(bodyBytes, &req)
+
+		missingParams := []string{}
+		for _, param := range paramList {
+			if req[param] == nil {
+				missingParams = append(missingParams, param)
+			}
+		}
+
+		if len(missingParams) > 0 {
+			c.AbortWithStatusJSON(400, gin.H{
+				"error": fmt.Sprintf(
+					"The following parameters are missing:- %s",
+					strings.Join(missingParams, ",")),
+			})
+		} else {
+			c.Next()
+		}
 	}
 }
