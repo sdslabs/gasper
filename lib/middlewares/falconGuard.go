6ace6049b8dc587b56c19a878da1cd5838f26389
Supratik Das <rick.das08@gmail.com>
2019-04-11 20:28:29 +0530

    fix: update falcon guard with new fixes

--- lib/middlewares/falconGuard.go
+++ lib/middlewares/falconGuard.go
@@ -4,26 +4,31 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	falconApi "github.com/supra08/falcon-client-golang"
+	"strings"
 )
 
 func user(cookie string) (string, error) {
 	config := falconApi.New("howl-MKUlTqXmtQHPEtPN", "0b3d4a96a621bf4c1d08ceddc49241482c96aa1b7e69c3e81f5f4190c80c0d8b", "http://falcon.sdslabs.local/access_token", "http://falcon.sdslabs.local/users/", "http://arceus.sdslabs.local/")
-	cookie = ""
-	user, err := falconApi.GetLoggedInUser(config, cookie)
+	hash := strings.Split(cookie, "=")[1]
+	fmt.Println(hash)
+	user, err := falconApi.GetLoggedInUser(config, hash)
 	if err != nil {
 		return "", fmt.Errorf("error in falcon client")
 	}
 	return user, nil
 }
 
-func FalconGuard(c *gin.Context) {
-	cookie := c.GetHeader("Cookie")
-	user, err := user(cookie)
-	if user == "" {
-		c.JSON(403, gin.H{
-			"error": err,
-		})
-		c.Abort()
-		return
+func FalconGuard() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cookie := c.GetHeader("Cookie")
+		user, err := user(cookie)
+		if user == "" {
+			c.JSON(403, gin.H{
+				"error": err,
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
 	}
 }
