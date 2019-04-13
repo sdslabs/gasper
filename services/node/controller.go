70d238b8d8730ed0083ee4007aabdef594f971dd
Vaibhav <vrongmeal@gmail.com>
2019-04-14 03:01:18 +0530

    refactor(node): extract and use pipeline from node controller

--- services/node/controller.go
+++ services/node/controller.go
@@ -2,56 +2,24 @@ package node
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/sdslabs/SWS/lib/api"
-	"github.com/sdslabs/SWS/lib/configs"
 	g "github.com/sdslabs/SWS/lib/gin"
 	"github.com/sdslabs/SWS/lib/mongo"
 	"github.com/sdslabs/SWS/lib/redis"
-	"github.com/sdslabs/SWS/lib/types"
 	"github.com/sdslabs/SWS/lib/utils"
 )
 
 // createApp function handles requests for making making new node app
 func createApp(c *gin.Context) {
-	var (
-		data   map[string]interface{}
-		execID string
-	)
+	var data map[string]interface{}
 	c.BindJSON(&data)
 
 	data["language"] = "node"
 
-	context := data["context"].(map[string]interface{})
-	appConf := &types.ApplicationConfig{
-		DockerImage:  utils.ServiceConfig["node"].(map[string]interface{})["image"].(string),
-		ConfFunction: configs.CreateNodeContainerConfig,
-	}
-
-	appEnv, resErr := api.SetupApplication(appConf, data)
+	resErr := pipeline(data)
 	if resErr != nil {
 		g.SendResponse(c, resErr, gin.H{})
 		return
 	}
-
-	// Perform npm install in the container
-	if data["npm"].(bool) {
-		execID, resErr = installPackages(appEnv)
-		if resErr != nil {
-			g.SendResponse(c, resErr, gin.H{})
-			return
-		}
-		data["execID"] = execID
-	}
-
-	index := context["index"].(string)
-
-	// Start app using pm2 in the container
-	execID, resErr = startApp(index, appEnv)
-	if resErr != nil {
-		g.SendResponse(c, resErr, gin.H{})
-		return
-	}
-	data["execID"] = execID
 
 	documentID, err := mongo.RegisterApp(data)
 
