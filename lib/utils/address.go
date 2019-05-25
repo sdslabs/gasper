6d290afed2993c7fa7892e0d9ef0adf495b46655
Alphadose <anish.mukherjee1996@gmail.com>
2019-05-25 23:59:49 +0530

    feat(*): add offline mode functionality

--- lib/utils/address.go
+++ lib/utils/address.go
@@ -2,10 +2,15 @@ package utils
 
 import (
 	"net"
+
+	"github.com/sdslabs/SWS/lib/configs"
 )
 
 // GetOutboundIP returns the preferred outbound IP of this machine
 func GetOutboundIP() string {
+	if configs.SWSConfig["offlineMode"].(bool) {
+		return "0.0.0.0"
+	}
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		panic("The machine is not connected to any network")
