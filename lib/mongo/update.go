8b888cdfd7cedb24f72d8fc0014e5b2aa38ad5ea
Alphadose <anish.mukherjee1996@gmail.com>
2019-05-25 23:40:56 +0530

    feat(mongo): add functionality to update multiple documents

--- lib/mongo/update.go
+++ lib/mongo/update.go
@@ -2,6 +2,7 @@ package mongo
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/mongodb/mongo-go-driver/bson"
@@ -13,13 +14,39 @@ func UpdateOne(collectionName string, filter bson.M, data bson.M) interface{} {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	res := collection.FindOneAndUpdate(ctx, filter, bson.M{"$set": data}, nil)
+	return res
+}
+
+// UpdateApp is an abstraction over UpdateOne which updates an application in mongoDB
+func UpdateApp(filter bson.M, data bson.M) interface{} {
+	return UpdateOne("apps", filter, data)
+}
+
+// UpdateMany updates multiple documents in the mongoDB collection
+func UpdateMany(collectionName string, filter bson.M, data bson.M) interface{} {
+	collection := link.Collection(collectionName)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	res, err := collection.UpdateMany(ctx, filter, bson.M{"$set": data}, nil)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 	return res
 }
 
-// UpdateApp is an abstraction over UpdateOne which deletes an application from mongoDB
-func UpdateApp(filter bson.M, data bson.M) interface{} {
-	return UpdateOne("apps", filter, data)
+// UpdateApps is an abstraction over UpdateMany which updates multiple applications in mongoDB
+func UpdateApps(filter bson.M, data bson.M) interface{} {
+	return UpdateMany("apps", filter, data)
+}
+
+// UpdateHostIP updates the application's host IP address
+func UpdateHostIP(oldIP, newIP string) interface{} {
+	return UpdateApps(
+		map[string]interface{}{
+			"hostIP": oldIP,
+		},
+		map[string]interface{}{
+			"hostIP": newIP,
+		},
+	)
 }
