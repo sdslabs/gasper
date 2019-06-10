0491fd59d988312641fa9a0d3789c75d6c585162
Supratik Das <rick.das08@gmail.com>
2019-06-10 23:12:08 +0530

    chore(lib/gin/container.go): add handlers for retrieving mysql logs and restarting mysql service in the container

--- lib/gin/container.go
+++ lib/gin/container.go
@@ -1,10 +1,12 @@
 package gin
 
 import (
+	"github.com/docker/docker/client"
 	"github.com/gin-gonic/gin"
 	"github.com/sdslabs/SWS/lib/docker"
 	"github.com/sdslabs/SWS/lib/types"
 	"github.com/sdslabs/SWS/lib/utils"
+	"golang.org/x/net/context"
 )
 
 // FetchLogs returns the container logs in a JSON format
@@ -40,6 +42,38 @@ func FetchLogs(c *gin.Context) {
 	})
 }
 
+// FetchMysqlContainerLogs returns the mysql container logs in a JSON format
+func FetchMysqlContainerLogs(c *gin.Context) {
+	queries := c.Request.URL.Query()
+	filter := utils.QueryToFilter(queries)
+
+	if filter["tail"] == nil {
+		filter["tail"] = "-1"
+	}
+
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	data, err := docker.ReadLogs(ctx, cli, "mysql", filter["tail"].(string))
+
+	if err != nil && err.Error() != "EOF" {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"data": data,
+	})
+}
+
 // ReloadServer reloads the nginx server
 func ReloadServer(c *gin.Context) {
 	app := c.Param("app")
@@ -54,6 +88,30 @@ func ReloadServer(c *gin.Context) {
 
 	cmd := []string{"nginx", "-s", "reload"}
 	_, err = docker.ExecDetachedProcess(appEnv.Context, appEnv.Client, app, cmd)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"success": true,
+	})
+}
+
+// ReloadMysqlService reloads the Mysql service in the container
+func ReloadMysqlService(c *gin.Context) {
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	cmd := []string{"nginx", "-s", "reload"}
+	_, err = docker.ExecDetachedProcess(ctx, cli, "mysql", cmd)
 	if err != nil {
 		c.JSON(500, gin.H{
 			"error": err.Error(),
