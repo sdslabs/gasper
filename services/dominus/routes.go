7d531f7eee3f88a0abf43b375d74dbcb64c480f8
Karanpreet Singh <karansingh0299@gmail.com>
2019-07-25 20:01:53 +0530

    use cors middleware in routes.go

--- services/dominus/routes.go
+++ services/dominus/routes.go
@@ -9,10 +9,10 @@ import (
 var Router = gin.NewEngine()
 
 func init() {
-	Router.POST("/:service", middlewares.FalconGuard(), createApp)
-	Router.GET("/", middlewares.FalconGuard(), fetchDocs)
-	Router.GET("/:app", middlewares.FalconGuard(), gin.FetchAppInfo)
-	Router.GET("/:app/:action", middlewares.FalconGuard(), execute)
+	Router.POST("/:service", middlewares.FalconGuard(), middlewares.CORSMiddleware(), createApp)
+	Router.GET("/", middlewares.FalconGuard(), middlewares.CORSMiddleware(), fetchDocs)
+	Router.GET("/:app", middlewares.FalconGuard(), middlewares.CORSMiddleware(), gin.FetchAppInfo)
+	Router.GET("/:app/:action", middlewares.FalconGuard(), middlewares.CORSMiddleware(), execute)
 	// Router.PUT("/", updateApp)
 	// Router.DELETE("/", deleteApp)
 }
