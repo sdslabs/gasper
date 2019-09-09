a720ec2c4f1bfe1a43927250baba9446be5059de
alphadose <anish.mukherjee1996@gmail.com>
2019-09-10 03:07:26 +0530

    feat(lib/mongo): add upsert functionality

--- lib/mongo/update.go
+++ lib/mongo/update.go
@@ -6,20 +6,27 @@ import (
 	"time"
 
 	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/mongo/options"
 )
 
 // UpdateOne updates a document in the mongoDB collection
-func UpdateOne(collectionName string, filter bson.M, data bson.M) interface{} {
+func UpdateOne(collectionName string, filter bson.M, data bson.M, option *options.FindOneAndUpdateOptions) interface{} {
 	collection := link.Collection(collectionName)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	res := collection.FindOneAndUpdate(ctx, filter, bson.M{"$set": data}, nil)
+	res := collection.FindOneAndUpdate(ctx, filter, bson.M{"$set": data}, option)
 	return res
 }
 
 // UpdateInstance is an abstraction over UpdateOne which updates an application in mongoDB
 func UpdateInstance(filter bson.M, data bson.M) interface{} {
-	return UpdateOne(InstanceCollection, filter, data)
+	return UpdateOne(InstanceCollection, filter, data, nil)
+}
+
+// UpsertInstance is an abstraction over UpdateOne which updates an application in mongoDB
+// or inserts it if the corresponding document doesn't exist
+func UpsertInstance(filter bson.M, data bson.M) interface{} {
+	return UpdateOne(InstanceCollection, filter, data, options.FindOneAndUpdate().SetUpsert(true))
 }
 
 // UpdateMany updates multiple documents in the mongoDB collection
