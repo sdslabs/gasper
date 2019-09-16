8f2d76bca06d00ce1d47031127fdb129092eedbe
alphadose <anish.mukherjee1996@gmail.com>
2019-09-16 09:08:26 +0530

    feat(lib/utils): add terminate functionality to scheduler

--- lib/utils/scheduler.go
+++ lib/utils/scheduler.go
@@ -6,6 +6,7 @@ import "time"
 type Scheduler struct {
 	interval time.Duration
 	task     func()
+	mutex    bool
 }
 
 // NewScheduler returns a pointer to a Scheduler object
@@ -18,13 +19,26 @@ func NewScheduler(interval time.Duration, task func()) *Scheduler {
 
 // Run starts scheduling the given task
 func (s *Scheduler) Run() {
+	if s.mutex {
+		return
+	}
+	s.mutex = true
 	ticker := time.NewTicker(s.interval)
 	for range ticker.C {
-		go s.task()
+		if s.mutex {
+			go s.task()
+		} else {
+			return
+		}
 	}
 }
 
 // RunAsync starts scheduling the given task in a non-blocking manner
 func (s *Scheduler) RunAsync() {
 	go s.Run()
+}
+
+// Terminate stops the scheduler
+func (s *Scheduler) Terminate() {
+	s.mutex = false
 }
