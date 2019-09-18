eb4214c90db0298ff3ab73b25cd669641722d294
Supratik Das <rick.das08@gmail.com>
2019-09-18 17:26:22 +0530

    feat(services/dominus/cleaner.go): add reschedule instance module for redeploying applications in different hosts

--- services/dominus/cleaner.go
+++ services/dominus/cleaner.go
@@ -3,19 +3,39 @@ package dominus
 import (
 	"time"
 
-	"github.com/sdslabs/SWS/lib/configs"
+	"github.com/sdslabs/SWS/lib/commons"
+	"github.com/sdslabs/SWS/lib/mongo"
 	"github.com/sdslabs/SWS/lib/redis"
 	"github.com/sdslabs/SWS/lib/utils"
 )
+
+// rescheduleInstance redeploys down instances on least loaded servers
+func rescheduleInstance(apps []map[string]interface{}, service string) {
+	for _, app := range apps {
+		instanceURLs, err := redis.GetLeastLoadedInstances(service, 1)
+		if err != nil {
+			fmt.Println(err)
+		}
+		app["hostIP"] = instanceURLs[0]
+		commons.Deployer(app, instanceURLs[0], service)
+	}
+}
 
 // inspectInstance checks whether a given instance is alive or not and deletes that instance
 // if it is dead
 func inspectInstance(service, instance string) {
 	if utils.NotAlive(instance) {
+		apps := mongo.FetchAppInfo(
+			map[string]interface{}{
+				"language": service,
+				"hostIP":   utils.HostIP,
+			},
+		)
 		err := redis.RemoveServiceInstance(service, instance)
 		if err != nil {
 			utils.LogError(err)
 		}
+		go rescheduleInstance(apps, service)
 	}
 }
 
