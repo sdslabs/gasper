389d0c7e4e4f295b09c14fa470444e8f221cdf1e
alphadose <anish.mukherjee1996@gmail.com>
2019-10-19 17:05:17 +0530

    refactor(lib/mongo): remove duplicate function argument types

--- lib/mongo/update.go
+++ lib/mongo/update.go
@@ -9,7 +9,7 @@ import (
 )
 
 // UpdateOne updates a document in the mongoDB collection
-func UpdateOne(collectionName string, filter types.M, data types.M, option *options.FindOneAndUpdateOptions) error {
+func UpdateOne(collectionName string, filter, data types.M, option *options.FindOneAndUpdateOptions) error {
 	collection := link.Collection(collectionName)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -18,29 +18,29 @@ func UpdateOne(collectionName string, filter types.M, data types.M, option *opti
 }
 
 // UpdateInstance is an abstraction over UpdateOne which updates an application in mongoDB
-func UpdateInstance(filter types.M, data types.M) error {
+func UpdateInstance(filter, data types.M) error {
 	return UpdateOne(InstanceCollection, filter, data, nil)
 }
 
 // UpsertInstance is an abstraction over UpdateOne which updates an application in mongoDB
 // or inserts it if the corresponding document doesn't exist
-func UpsertInstance(filter types.M, data types.M) error {
+func UpsertInstance(filter, data types.M) error {
 	return UpdateOne(InstanceCollection, filter, data, options.FindOneAndUpdate().SetUpsert(true))
 }
 
 // UpdateUser is an abstraction over UpdateOne which updates an application in mongoDB
-func UpdateUser(filter types.M, data types.M) error {
+func UpdateUser(filter, data types.M) error {
 	return UpdateOne(UserCollection, filter, data, nil)
 }
 
 // UpsertUser is an abstraction over UpdateOne which updates an application in mongoDB
 // or inserts it if the corresponding document doesn't exist
-func UpsertUser(filter types.M, data types.M) error {
+func UpsertUser(filter, data types.M) error {
 	return UpdateOne(UserCollection, filter, data, options.FindOneAndUpdate().SetUpsert(true))
 }
 
 // UpdateMany updates multiple documents in the mongoDB collection
-func UpdateMany(collectionName string, filter types.M, data types.M) (interface{}, error) {
+func UpdateMany(collectionName string, filter, data types.M) (interface{}, error) {
 	collection := link.Collection(collectionName)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -52,6 +52,6 @@ func UpdateMany(collectionName string, filter types.M, data types.M) (interface{
 }
 
 // UpdateInstances is an abstraction over UpdateMany which updates multiple applications in mongoDB
-func UpdateInstances(filter types.M, data types.M) (interface{}, error) {
+func UpdateInstances(filter, data types.M) (interface{}, error) {
 	return UpdateMany(InstanceCollection, filter, data)
 }
