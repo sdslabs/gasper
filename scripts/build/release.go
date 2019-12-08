8e21a81013727387884e493a7bcfa88840bd99e4
alphadose <anish.mukherjee1996@gmail.com>
2019-12-09 03:51:36 +0530

    chore(scripts): update release script

--- scripts/build/release.go
+++ scripts/build/release.go
@@ -10,10 +10,10 @@ import (
 const releaseDir = "releases"
 
 var releaseMap = map[string][]string{
-	"darwin":  []string{"amd64"},
-	"linux":   []string{"386", "amd64", "arm", "arm64", "ppc64le"},
+	"darwin":  []string{"386", "amd64"},
+	"linux":   []string{"386", "amd64", "arm64", "ppc64le"},
 	"freebsd": []string{"386", "amd64"},
-	"openbsd": []string{"amd64"},
+	"openbsd": []string{"386", "amd64"},
 	"windows": []string{"386", "amd64"},
 }
 
