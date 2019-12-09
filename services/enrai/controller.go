a7f91558a75d6c4ee78926e3614a7cc3272cea72
alphadose <anish.mukherjee1996@gmail.com>
2019-12-09 05:31:42 +0530

    refactor(services/enrai): group variables into a single declaration and make overall function more readable

--- services/enrai/controller.go
+++ services/enrai/controller.go
@@ -20,19 +20,20 @@ const (
 	SSLServiceName = types.EnraiSSL
 )
 
-// storage stores the reverse proxy records in the form of Key : Value pairs
-// with Application Name as the key and its URL(IP:Port) as the value
-var storage = types.NewRecordStorage()
-
-// balancedInstances are the services for which Enrai load balances the request among multiple instances
-var balancedInstances = []string{
-	types.Kaze,
-}
-
-// kazeBalancer load balances requests among multiple kaze instances
-var kazeBalancer = types.NewLoadBalancer()
-
 var (
+	// storage stores the reverse proxy records in the form of Key : Value pairs
+	// with Application Name as the key and its URL(IP:Port) as the value
+	storage = types.NewRecordStorage()
+
+	// balancedInstances are the services for which Enrai load balances the
+	// request among multiple instances
+	balancedInstances = []string{
+		types.Kaze,
+	}
+
+	// kazeBalancer load balances requests among multiple kaze instances
+	kazeBalancer = types.NewLoadBalancer()
+
 	// Root domain name for validating host names
 	rootDomain = fmt.Sprintf(".%s", configs.GasperConfig.Domain)
 
@@ -42,39 +43,41 @@ var (
 
 // reverseProxy sets up the reverse proxy from the given domain to the target IP
 func reverseProxy(c *gin.Context) {
-	if strings.HasSuffix(c.Request.Host, rootDomain) || strings.HasSuffix(c.Request.Host, rootDomainWithPort) {
-		name := strings.Split(c.Request.Host, ".")[0]
-		var target string
-		var success bool
-
-		if utils.Contains(balancedInstances, name) {
-			target, success = kazeBalancer.Get()
-		} else {
-			target, success = storage.Get(name)
-		}
-
-		if !success {
-			c.AbortWithStatusJSON(503, gin.H{
-				"success": false,
-				"message": "No such application exists",
-			})
-			return
-		}
-
-		director := func(req *http.Request) {
-			req.URL.Scheme = "http"
-			req.URL.Host = target
-			req.Host = target
-		}
-		proxy := &httputil.ReverseProxy{Director: director}
-		proxy.ServeHTTP(c.Writer, c.Request)
+	if !strings.HasSuffix(c.Request.Host, rootDomain) && !strings.HasSuffix(c.Request.Host, rootDomainWithPort) {
+		c.AbortWithStatusJSON(403, gin.H{
+			"success": false,
+			"message": "Incorrect root domain",
+		})
 		return
 	}
 
-	c.AbortWithStatusJSON(403, gin.H{
-		"success": false,
-		"message": "Incorrect root domain",
-	})
+	name := strings.Split(c.Request.Host, ".")[0]
+	var target string
+	var success bool
+
+	if utils.Contains(balancedInstances, name) {
+		target, success = kazeBalancer.Get()
+	} else {
+		target, success = storage.Get(name)
+	}
+
+	if !success {
+		c.AbortWithStatusJSON(503, gin.H{
+			"success": false,
+			"message": "No such application exists",
+		})
+		return
+	}
+
+	director := func(req *http.Request) {
+		req.URL.Scheme = "http"
+		req.URL.Host = target
+		req.Host = target
+	}
+
+	proxy := &httputil.ReverseProxy{Director: director}
+	proxy.ServeHTTP(c.Writer, c.Request)
+	return
 }
 
 // NewService returns a new instance of the current microservice
