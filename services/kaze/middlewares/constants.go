e1462236fb941e8eab7220e6ee43ed4afd1d8b70
Supratik Das <rick.das08@gmail.com>
2020-04-01 14:23:15 +0530

    lint(services/kaze/middlewares/constants.go): fix lint in comment

--- services/kaze/middlewares/constants.go
+++ services/kaze/middlewares/constants.go
@@ -1,8 +1,8 @@
 package middlewares
 
 const (
-	// Request param label for application instance type
+	// AppReqParam is Request param label for application instance type
 	AppReqParam = "app"
-	// Request param label for database instance type
+	// DBReqParam Request param label for database instance type
 	DBReqParam = "db"
 )
