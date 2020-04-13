33a870568e44ef898a39046c51aead7c991d0d6e
aryaman-titan <aryamanbehera@gmail.com>
2020-04-14 00:44:12 +0530

    refactor(lib/database/handler.go): volume mount for redis database

--- lib/database/handler.go
+++ lib/database/handler.go
@@ -20,69 +20,48 @@ type containerHandler struct {
 }
 
 
+var storepath, _ = os.Getwd()
+
+var databaseMap = map[string]*containerHandler{
+	types.MongoDB: {
+		dockerImage: configs.ImageConfig.Mongodb,
+		port:        strconv.Itoa(configs.ServiceConfig.Kaen.MongoDB.ContainerPort),
+		env:         configs.ServiceConfig.Kaen.MongoDB.Env,
+		workdir:     "/data/db",
+		storedir:    filepath.Join(storepath, "mongodb-storage"),
+		containerID: docker.CreateMongoDBContainer,
+	},
+	types.MongoDBGasper: {
+		dockerImage: configs.ImageConfig.Mongodb,
+		port:        strconv.Itoa(configs.ServiceConfig.Kaze.MongoDB.ContainerPort),
+		env:         configs.ServiceConfig.Kaze.MongoDB.Env,
+		workdir:     "/data/db",
+		storedir:    filepath.Join(storepath, "gasper-mongodb-storage"),
+		containerID: docker.CreateMongoDBContainer,
+	},
+	types.MySQL: {
+		dockerImage: configs.ImageConfig.Mysql,
+		port:        strconv.Itoa(configs.ServiceConfig.Kaen.MySQL.ContainerPort),
+		env:         configs.ServiceConfig.Kaen.MySQL.Env,
+		workdir:     "/var/lib/mysql",
+		storedir:    filepath.Join(storepath, "mysql-storage"),
+		containerID: docker.CreateMysqlContainer,
+	},
+	types.RedisGasper: {
+		dockerImage: configs.ImageConfig.Redis,
+		port:        strconv.Itoa(configs.ServiceConfig.Kaze.Redis.ContainerPort),
+		env:         configs.ServiceConfig.Kaze.Redis.Env,
+		workdir:     "/data/",
+		storedir:    filepath.Join(storepath, "gasper-redis-storage"),
+		containerID: docker.CreateRedisContainer,
+	},
+}
+
 // SetupDBInstance sets up containers for database
 func SetupDBInstance(databaseType string) (string, types.ResponseError) {
 
-	storepath, _ := os.Getwd()
-
 	var containerID string
 	var err error
-
-	switch databaseType {
-	case types.MongoDB:
-		{
-			dockerImage := configs.ImageConfig.Mongodb
-			port := strconv.Itoa(configs.ServiceConfig.Kaen.MongoDB.ContainerPort)
-			env := configs.ServiceConfig.Kaen.MongoDB.Env
-			workdir := "/data/db"
-			storedir := filepath.Join(storepath, "mongodb-storage")
-			containerID, err = GetDockerConfig(dockerImage, port, workdir, storedir, env, databaseType)
-		}
-	case types.MySQL:
-		{
-			dockerImage := configs.ImageConfig.Mysql
-			port := strconv.Itoa(configs.ServiceConfig.Kaen.MySQL.ContainerPort)
-			env := configs.ServiceConfig.Kaen.MySQL.Env
-			workdir := "/var/lib/mysql"
-			storedir := filepath.Join(storepath, "mysql-storage")
-			containerID, err = GetDockerConfig(dockerImage, port, workdir, storedir, env, databaseType)
-		}
-	case types.PostgreSQL:
-		{
-			dockerImage := configs.ImageConfig.Postgresql
-			port := strconv.Itoa(configs.ServiceConfig.Kaen.PostgreSQL.ContainerPort)
-			env := configs.ServiceConfig.Kaen.PostgreSQL.Env
-			workdir := "/var/lib/postgresql/data"
-			storedir := filepath.Join(storepath, "postgresql-storage")
-			containerID, err = docker.CreatePostgreSQLContainer(
-				dockerImage,
-				port,
-				workdir,
-				storedir,
-				env,
-				databaseType)
-		}
-	case types.MongoDBGasper:
-		{
-			dockerImage := configs.ImageConfig.Mongodb
-			port := strconv.Itoa(configs.ServiceConfig.Kaze.MongoDB.ContainerPort)
-			env := configs.ServiceConfig.Kaze.MongoDB.Env
-			workdir := "/data/db"
-			storedir := filepath.Join(storepath, "gasper-mongodb-storage")
-			containerID, err = GetDockerConfig(dockerImage, port, workdir, storedir, env, databaseType)
-		}
-	case types.RedisGasper:
-		{
-			dockerImage := configs.ImageConfig.Redis
-			port := strconv.Itoa(configs.ServiceConfig.Kaze.Redis.ContainerPort)
-			env := configs.ServiceConfig.Kaze.Redis.Env
-			workdir := "/data/db"
-			storedir := filepath.Join(storepath, "gasper-redis-storage")
-			containerID, err = GetDockerConfig(dockerImage, port, workdir, storedir, env, databaseType)
-		}
-	default:
-		return "", types.NewResErr(500, "invalid database type provided", errors.New("invalid database type provided"))
-	}
 
 	containerID, err = databaseMap[databaseType].containerID(databaseMap[databaseType].dockerImage,
 		databaseMap[databaseType].port,
