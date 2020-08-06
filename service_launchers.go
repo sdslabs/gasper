b1bb042433ce4cff042cf5e7ac139c9d78bfa918
Angad Kambli <angadkambli@gmail.com>
2020-08-06 18:58:50 +0530

    cleanup(service-launcher.go): remove mysql container deployment which was put in for testing
    remove uneeded mysql container deployment from service-launcher.go
    
    database containers deployment still doesn't work properly

--- service_launchers.go
+++ service_launchers.go
@@ -131,8 +131,6 @@ func startMasterService() error {
 		checkAndPullImages(configs.ImageConfig.Redis)
 		setupDatabaseContainer(types.RedisGasper)
 	}
-	checkAndPullImages(configs.ImageConfig.Mysql)
-	setupDatabaseContainer(types.MySQL)
 
 	return buildHTTPServer(master.NewService(), configs.ServiceConfig.Master.Port).ListenAndServe()
 }
