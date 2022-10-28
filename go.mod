f0ac686fe0360e1e2da05796fa1212b331b08eec
Shreya Bhagat <shreyabhagat2002@gmail.com>
2022-10-28 19:24:32 +0530

    feat(*): add endpoint to create github repo and push code to it

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/sdslabs/gasper
 
 require (
 	github.com/BurntSushi/toml v0.3.1
-	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/NYTimes/gziphandler v1.1.1 // indirect
 	github.com/alphadose/gotty v0.0.0-20191208194000-a33c4414c39e
 	github.com/anmitsu/go-shlex v0.0.0-20200502080107-070676123096 // indirect
@@ -19,29 +18,30 @@ require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/gliderlabs/ssh v0.3.0
+	github.com/go-git/go-git/v5 v5.4.2
 	github.com/go-redis/redis v6.15.7+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.4.1
+	github.com/google/go-github/v41 v41.0.0
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/go-multierror v1.1.0 // indirect
 	github.com/jackc/pgx/v4 v4.6.0
+	github.com/joho/godotenv v1.3.0
 	github.com/klauspost/compress v1.10.5 // indirect
 	github.com/kr/pty v1.1.8
 	github.com/miekg/dns v1.1.29
 	github.com/onsi/ginkgo v1.10.3 // indirect
 	github.com/onsi/gomega v1.7.1 // indirect
 	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sdslabs/gin-jwt v1.0.0
-	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/urfave/cli v1.22.4 // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.3.3
-	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79
-	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
+	golang.org/x/net v0.0.0-20210326060303-6b1517762897
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a
-	golang.org/x/sys v0.0.0-20200509044756-6aff5f38e54f // indirect
 	google.golang.org/genproto v0.0.0-20200507105951-43844f6eee31 // indirect
 	google.golang.org/grpc v1.29.1
 	gopkg.in/src-d/go-git.v4 v4.13.1
---
+++ services/master/controllers/github.go
@@ -0,0 +1,158 @@
+package controllers
+
+import (
+	"strings"
+
+	"os"
+	"path/filepath"
+
+	"context"
+	_ "io/ioutil"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/google/go-github/v41/github"
+
+	"github.com/sdslabs/gasper/lib/utils"
+	"golang.org/x/oauth2"
+)
+
+func CreateRepository(c *gin.Context) {
+	filters := utils.QueryToFilter(c.Request.URL.Query())
+	repoName, pathToApplication := filters["name"], filters["path"]
+	repository, err := CreateRepositoryGithub(repoName.(string))
+
+	if err != nil {
+		c.AbortWithStatusJSON(400, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+	}
+	repo, err := GitPush(pathToApplication.(string), repository.GetCloneURL())
+
+	if err != nil {
+		c.AbortWithStatusJSON(400, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+	}
+
+	c.JSON(200, gin.H{
+		"success": true,
+		"message": repo,
+	})
+}
+
+func GoDotEnvVariable(key string) string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		panic(err)
+	}
+	environmentPath := filepath.Join(strings.TrimRight(dir, "/tmp"), ".env")
+	err = godotenv.Load(environmentPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return os.Getenv(key)
+}
+
+func CreateRepositoryGithub(repoName string) (*github.Repository, error) {
+	tc := oauth2.NewClient(
+		context.Background(),
+		oauth2.StaticTokenSource(
+			&oauth2.Token{
+				AccessToken: GoDotEnvVariable("PAT"), //PAT
+			},
+		),
+	)
+	client := github.NewClient(tc)
+	repo := &github.Repository{
+		Name:    github.String(repoName),
+		Private: github.Bool(true),
+	}
+	repo, _, err := client.Repositories.Create(context.Background(), "", repo)
+	return repo, err
+}
+
+func GitInit(directoryPath string) (*git.Repository, error) {
+	// fmt.Println("GITINIT DONE")
+	var (
+		err error
+	)
+	_, err = os.Stat(directoryPath)
+	if err != nil {
+		return nil, err
+	}
+	repository, err := git.PlainInit(directoryPath, false)
+	return repository, err
+}
+
+func GitPush(pathToApplication string, repoURL string) (*git.Repository, error) {
+	var firstInit bool
+	repo, err := git.PlainOpen(pathToApplication)
+	if err != nil {
+		firstInit = true
+		repo, err = GitInit(pathToApplication)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		firstInit = false
+	}
+	_, err = repo.Remote("origin")
+	if err != nil {
+		_, err = repo.CreateRemote(&config.RemoteConfig{
+			Name: "origin",
+			URLs: []string{repoURL},
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+	w, _ := repo.Worktree()
+	if firstInit {
+		err = w.AddGlob(".")
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err = w.Add(".")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	_, _ = w.Commit("latest commit", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  GoDotEnvVariable("USERNAME"),
+			Email: GoDotEnvVariable("EMAIL"),
+			When:  time.Now(),
+		},
+	})
+
+	auth := &http.BasicAuth{
+		Username: GoDotEnvVariable("USERNAME"),
+		Password: GoDotEnvVariable("PAT"),
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	err = repo.Push(&git.PushOptions{
+		RemoteName: "origin",
+		Auth:       auth,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return repo, err
+}
