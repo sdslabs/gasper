0dc29ccac69e362f5197cfd638089fb7b3fbe95d
Darshan Kumar <itsdarshankumar@gmail.com>
2023-02-21 21:03:16 +0530

    fix depricated go get methods

--- scripts/build/install_fresh.sh
+++ scripts/build/install_fresh.sh
@@ -11,7 +11,7 @@ printf "🔨 Installing fresh\n"
 mkdir -p bin
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 cd $tmp_dir
-GOPATH=$tmp_dir go get github.com/pilu/fresh
+GOPATH=$tmp_dir go install github.com/pilu/fresh@latest
 cp $tmp_dir/bin/fresh $project_dir/bin/fresh
 rm -rf $tmp_dir
 
