50d9be26fa9710c4d611bdaa30c278e44d43d46a
Ishan <gargishan.1211@gmail.com>
2025-01-13 21:30:19 +0530

    Fix delete app and custom app search route in admin

--- lib/utils/strings.go
+++ lib/utils/strings.go
@@ -1,12 +1,27 @@
 package utils
 
-import "github.com/sdslabs/gasper/types"
+import (
+	"strconv"
+	"github.com/sdslabs/gasper/types"
+)
 
 // QueryToFilter filters out queries from the URL parameters
 func QueryToFilter(queries map[string][]string) types.M {
 	filter := make(types.M)
 
 	for key, value := range queries {
+		if(key == "git_url"){
+			filter["git.repo_url"] = value[0]
+			continue
+		}
+		if(key == "container_port"){
+			containerPort, err := strconv.Atoi(value[0])
+			if err != nil {
+				return nil
+			}
+			filter["container_port"] = containerPort
+			continue
+		}
 		filter[key] = value[0]
 	}
 
