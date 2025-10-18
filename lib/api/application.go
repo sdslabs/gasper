0ab9d1403902d7f39e7f13eae3b9853bd1a8d5ce
karma <captainav0608@gmail.com>
2025-10-19 01:46:45 +0530

    [fix] run build and start command upon app re creation

--- lib/api/application.go
+++ lib/api/application.go
@@ -62,6 +62,12 @@ func setupContainer(app types.Application, storedir string, setup chan types.Res
 		setup <- types.NewResErr(500, "container not started", err)
 		return
 	}
+	err = RunBuildAndStartCommands(app)
+	if err != nil {
+		setup <- types.NewResErr(500, "build and start commands fail", err)
+		return
+	}
+
 	setup <- nil
 }
 
