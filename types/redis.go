cfaa7874aff4042ae6ddc0c324abd87b800994e3
karma <captainav0608@gmail.com>
2025-12-16 12:28:35 +0530

    [fix] update validator bindings

--- types/redis.go
+++ types/redis.go
@@ -2,6 +2,6 @@ package types
 
 // InstanceBindings defines the struct for storing both the instance's server and node urls
 type InstanceBindings struct {
-	Node   string `json:"node" valid:"required~Field 'node' is required,matches(^([0-9]|[0-9][0-9]|[0-9][0-9][0-9])\\.([0-9]|[0-9][0-9]|[0-9][0-9][0-9])\\.([0-9]|[0-9][0-9]|[0-9][0-9][0-9])\\.([0-9]|[0-9][0-9]|[0-9][0-9][0-9]):([0-9]|[0-9][0-9]|[0-9][0-9][0-9][0-9]|[0-9][0-9][0-9][0-9][0-9])$)~Field 'node' must match format 'x.x.x.x:PORT'"`
+	Node   string `json:"node" validate:"required,hostname_port"`
 	Server string `json:"server"`
 }
